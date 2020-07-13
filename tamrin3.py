fc9c5a9cdc868bec7263c74efe005b06d57cc29a
amirerfanakbari <erfanakbari259@gmail.com>
2020-07-13 18:39:10 +0430

    create tamrin 3

---
+++ tamrin3.py
@@ -0,0 +1,5 @@
+hogug_karmand = int(input('hogug karmand:'))
+bymeh = hogug_karmand * 0.07
+maliyat = hogug_karmand * 0.1
+daryafti = hogug_karmand - bymeh - maliyat
+print('bymeh', bymeh, 'maliyat', maliyat, 'daryafti', daryafti)
